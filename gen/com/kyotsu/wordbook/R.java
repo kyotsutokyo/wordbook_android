d68d2b27209c762463be94bc8592433edc9eba05
howarder3 <howarder3@gmail.com>
2014-08-20 18:22:13 +0900

    4564

--- gen/com/kyotsu/wordbook/R.java
+++ gen/com/kyotsu/wordbook/R.java
@@ -20,82 +20,92 @@ public final class R {
         public static final int activity_horizontal_margin=0x7f040000;
     }
     public static final class drawable {
-        public static final int btn_army_glossy=0x7f020000;
-        public static final int btn_black=0x7f020001;
-        public static final int btn_black_glossy=0x7f020002;
-        public static final int btn_blue_glossy=0x7f020003;
-        public static final int btn_blue_matte=0x7f020004;
-        public static final int btn_blue_pink_glossy=0x7f020005;
-        public static final int btn_default_normal=0x7f020006;
-        public static final int btn_default_normal_blue=0x7f020007;
-        public static final int btn_default_normal_brown=0x7f020008;
-        public static final int btn_default_normal_disable=0x7f020009;
-        public static final int btn_default_normal_disable_focused=0x7f02000a;
-        public static final int btn_default_normal_green=0x7f02000b;
-        public static final int btn_default_normal_lblue=0x7f02000c;
-        public static final int btn_default_normal_pink=0x7f02000d;
-        public static final int btn_default_normal_red=0x7f02000e;
-        public static final int btn_default_normal_yellow=0x7f02000f;
-        public static final int btn_default_pressed=0x7f020010;
-        public static final int btn_default_selected=0x7f020011;
-        public static final int btn_green_glossy=0x7f020012;
-        public static final int btn_green_matte=0x7f020013;
-        public static final int btn_lightblue_glossy=0x7f020014;
-        public static final int btn_orange_matte=0x7f020015;
-        public static final int btn_pink_glossy=0x7f020016;
-        public static final int btn_pink_matte=0x7f020017;
-        public static final int btn_red_matte=0x7f020018;
-        public static final int btn_white_glossy=0x7f020019;
-        public static final int btn_white_matte=0x7f02001a;
-        public static final int btn_yellowgreen_glossy=0x7f02001b;
-        public static final int ic_launcher=0x7f02001c;
+        public static final int blue_button=0x7f020000;
+        public static final int btn_army_glossy=0x7f020001;
+        public static final int btn_black=0x7f020002;
+        public static final int btn_black_glossy=0x7f020003;
+        public static final int btn_blue_glossy=0x7f020004;
+        public static final int btn_blue_matte=0x7f020005;
+        public static final int btn_blue_pink_glossy=0x7f020006;
+        public static final int btn_default_normal=0x7f020007;
+        public static final int btn_default_normal_blue=0x7f020008;
+        public static final int btn_default_normal_brown=0x7f020009;
+        public static final int btn_default_normal_disable=0x7f02000a;
+        public static final int btn_default_normal_disable_focused=0x7f02000b;
+        public static final int btn_default_normal_green=0x7f02000c;
+        public static final int btn_default_normal_lblue=0x7f02000d;
+        public static final int btn_default_normal_pink=0x7f02000e;
+        public static final int btn_default_normal_red=0x7f02000f;
+        public static final int btn_default_normal_yellow=0x7f020010;
+        public static final int btn_default_pressed=0x7f020011;
+        public static final int btn_default_selected=0x7f020012;
+        public static final int btn_green_glossy=0x7f020013;
+        public static final int btn_green_matte=0x7f020014;
+        public static final int btn_lightblue_glossy=0x7f020015;
+        public static final int btn_orange_matte=0x7f020016;
+        public static final int btn_pink_glossy=0x7f020017;
+        public static final int btn_pink_matte=0x7f020018;
+        public static final int btn_red_matte=0x7f020019;
+        public static final int btn_white_glossy=0x7f02001a;
+        public static final int btn_white_matte=0x7f02001b;
+        public static final int btn_yellowgreen_glossy=0x7f02001c;
+        public static final int green_button=0x7f02001d;
+        public static final int ic_launcher=0x7f02001e;
+        public static final int yellow_button=0x7f02001f;
     }
     public static final class id {
-        public static final int CheckBox01=0x7f080014;
-        public static final int Password=0x7f080016;
-        public static final int Translation=0x7f08000b;
-        public static final int Translation_edit=0x7f08000c;
-        public static final int Word_edit=0x7f08000a;
-        public static final int action_Login=0x7f08001d;
-        public static final int action_remove=0x7f08001e;
-        public static final int action_remove_words=0x7f08001f;
-        public static final int action_settings=0x7f08001c;
-        public static final int btnAdd=0x7f080004;
-        public static final int btnCancel=0x7f080005;
-        public static final int btnShow=0x7f080003;
-        public static final int btnremove=0x7f080006;
-        public static final int btnsort=0x7f080002;
-        public static final int cancel=0x7f08000d;
-        public static final int date_edit=0x7f080013;
-        public static final int fix_word=0x7f08001b;
-        public static final int footer=0x7f080001;
-        public static final int header=0x7f080000;
-        public static final int listview=0x7f080007;
-        public static final int login=0x7f080018;
-        public static final int ok=0x7f08000e;
-        public static final int show_visible=0x7f080020;
-        public static final int text=0x7f080019;
-        public static final int textSeparator=0x7f08001a;
-        public static final int translation=0x7f080010;
-        public static final int user_id=0x7f080015;
-        public static final int user_password=0x7f080017;
-        public static final int word_edit=0x7f08000f;
-        public static final int word_name=0x7f080011;
-        public static final int word_translation=0x7f080012;
-        public static final int worddetail_item=0x7f080009;
-        public static final int worddetail_translation=0x7f080008;
+        public static final int CheckBox01=0x7f08001b;
+        public static final int Password=0x7f08001d;
+        public static final int Translation=0x7f080012;
+        public static final int Translation_edit=0x7f080013;
+        public static final int Word_edit=0x7f080011;
+        public static final int action_Login=0x7f080022;
+        public static final int action_remove=0x7f080023;
+        public static final int action_remove_words=0x7f080024;
+        public static final int action_settings=0x7f080021;
+        public static final int btnAdd=0x7f08000b;
+        public static final int btnCancel=0x7f08000c;
+        public static final int btnShow=0x7f08000a;
+        public static final int btnremove=0x7f08000d;
+        public static final int btnsort=0x7f080009;
+        public static final int cancel=0x7f080014;
+        public static final int cancel_login=0x7f080006;
+        public static final int date_edit=0x7f08001a;
+        public static final int fix_word=0x7f080020;
+        public static final int footer=0x7f080008;
+        public static final int forgot_password=0x7f080004;
+        public static final int header=0x7f080007;
+        public static final int listview=0x7f08000e;
+        public static final int login=0x7f080003;
+        public static final int ok=0x7f080015;
+        public static final int register=0x7f080005;
+        public static final int show_visible=0x7f080025;
+        public static final int text=0x7f08001e;
+        public static final int textSeparator=0x7f08001f;
+        public static final int textview=0x7f080000;
+        public static final int translation=0x7f080017;
+        public static final int user_id=0x7f08001c;
+        public static final int user_name=0x7f080001;
+        public static final int user_password=0x7f080002;
+        public static final int word_edit=0x7f080016;
+        public static final int word_name=0x7f080018;
+        public static final int word_translation=0x7f080019;
+        public static final int worddetail_item=0x7f080010;
+        public static final int worddetail_translation=0x7f08000f;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_word_detail=0x7f030001;
-        public static final int add_word=0x7f030002;
-        public static final int dialog_signin=0x7f030003;
-        public static final int listview_item_layout=0x7f030004;
-        public static final int login=0x7f030005;
-        public static final int remove=0x7f030006;
-        public static final int snippet_item1=0x7f030007;
-        public static final int snippet_item2=0x7f030008;
-        public static final int trans_edit=0x7f030009;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_word_detail=0x7f030002;
+        public static final int add_word=0x7f030003;
+        public static final int dialog_signin=0x7f030004;
+        public static final int listview_item_layout=0x7f030005;
+        public static final int login=0x7f030006;
+        public static final int login_dialog=0x7f030007;
+        public static final int remove=0x7f030008;
+        public static final int snippet_item1=0x7f030009;
+        public static final int snippet_item2=0x7f03000a;
+        public static final int trans_edit=0x7f03000b;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -117,13 +127,37 @@ public final class R {
         public static final int button_sort1=0x7f050010;
         public static final int button_sort2=0x7f050011;
         public static final int cancel=0x7f05000a;
+        public static final int cancel_login=0x7f050028;
+        public static final int err_alert=0x7f050029;
+        public static final int error=0x7f05001f;
+        public static final int forgot_password=0x7f050026;
         public static final int login=0x7f050012;
+        public static final int login_alert_1=0x7f05002a;
+        public static final int login_alert_2=0x7f05002b;
+        public static final int login_alert_3=0x7f05002c;
+        public static final int login_alert_4=0x7f05002d;
+        public static final int login_alert_5=0x7f05002e;
+        public static final int login_alert_6=0x7f05002f;
+        public static final int login_failed=0x7f05001e;
+        public static final int login_password_failed=0x7f05001d;
+        public static final int login_user_failed=0x7f05001c;
+        public static final int logout=0x7f05001b;
         public static final int menu_settings=0x7f050001;
+        public static final int offline_mode_click=0x7f050024;
+        public static final int offline_mode_text1=0x7f050023;
+        public static final int offline_mode_text2=0x7f050021;
+        public static final int offline_mode_text3=0x7f050022;
         public static final int ok=0x7f05000b;
+        public static final int online_mode_click=0x7f050025;
+        public static final int password=0x7f050019;
+        public static final int register=0x7f050027;
+        public static final int rememberLogin=0x7f05001a;
         public static final int remove=0x7f050004;
         public static final int remove_words=0x7f050016;
+        public static final int search_record_none=0x7f050020;
         public static final int show_visible=0x7f050017;
         public static final int translation_hint=0x7f050009;
+        public static final int user=0x7f050018;
         public static final int word_hint=0x7f050008;
     }
     public static final class style {
@@ -148,11 +182,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060001;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060002;
-        public static final int ButtonText=0x7f060000;
+        public static final int AppTheme=0x7f060001;
+        public static final int ButtonText=0x7f060002;
     }
 }
